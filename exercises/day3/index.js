7d568ecf650ed4e1b00db70cd882e85606d1c33c
Sneha-BiswasZWT <sneha@zealousweb.com>
2025-01-27 13:04:01 +0530

    removed unsed code and added comments

--- exercises/day3/index.js
+++ exercises/day3/index.js
@@ -1,12 +1,11 @@
 const express= require('express');
 const app= express();
-const mainController = require("./controller")
 const apiRoutes = require("./routes/router");
 const log = require("./middlewares/log")
 
 app.use(express.json());
 app.use(log);
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({extended : false}));//for form data
 app.get('/', (req,res)=>
 {
     res.send("Welcome to the User Management API!")
