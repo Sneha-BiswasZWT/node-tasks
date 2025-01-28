e36194f93ca2e5edaadce9189ac0e79f38b34d0a
Sneha-BiswasZWT <sneha@zealousweb.com>
2025-01-28 19:02:58 +0530

    modified file name for better execution

--- exercises/day3/middlewares/fileUploader.js
+++ exercises/day3/middlewares/fileUploader.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     cb(null, uploadFolder);
   },
   filename: function (req, file, cb) {
-    cb(null, `${file.originalname}-${Date.now()}`);
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 
 });
